a925a01c4b0b8ef55ad104e922a33d0a5b54efff
JOR-Attack <jorenbulo@gmail.com>
2025-05-13 21:10:45 -0500

    Initial

---
+++ script.js
@@ -0,0 +1,44 @@
+// Efecto de Escritura para el Subtítulo
+const typedTextSpan = document.getElementById("typed-text");
+const textArray = [
+  "Profesional de Sistemas",
+  "Desarrollador Web",
+  "Experto en Redes y Bases de Datos",
+];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Año actual en el footer
+document.getElementById("current-year").textContent = new Date().getFullYear();
